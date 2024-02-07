1686584abb1a764e98750e88032bdf2ef8b815bb
MarcusAdriano <marcusadriano.pereira@gmail.com>
2024-02-07 00:08:06 -0300

    Downgrade fiber + configuracao zerolog

--- cmd/rinha/main.go
+++ cmd/rinha/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v3"
+	"os"
+
+	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -9,16 +12,26 @@ import (
 func main() {
 
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).
+		With().
+		Timestamp().
+		Logger()
 
 	app := fiber.New()
+	app.Use(fiberzerolog.New(
+		fiberzerolog.Config{
+			Logger: &log.Logger,
+		},
+	))
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Hello Rinha de backend 2024!!")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, rinha de backend 2024 q1")
 	})
 
 	err := app.Listen(":3000")
 	if err != nil {
 		log.Fatal().Msgf("Error: %s", err)
 	}
-	log.Info().Msg("App running on port :3000")
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module github.com/marcusadriano/rinha-de-backend-2024-q1
 go 1.21.4
 
 require (
+	github.com/gofiber/contrib/fiberzerolog v0.2.3
+	github.com/gofiber/fiber/v2 v2.52.0
+	github.com/rs/zerolog v1.32.0
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/gofiber/fiber/v3 v3.0.0-20240205091129-9b0a99ba27fc // indirect
-	github.com/gofiber/utils/v2 v2.0.0-beta.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.32.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
